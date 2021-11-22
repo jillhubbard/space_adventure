b7509ef3244316cbef7a519da30486bb713dcbeb
Xander Bailey <xander.bailey@gmail.com>
2021-11-21 18:05:15 -0800

    Cookbook style abstractions

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ A simple program to explore the solar system a bit.
 
 import json
 
-def main():
+goto_random_planet = ""
+
+
+def print_welcome():
     print("Welcome to the Solar System!")
 
+
+def get_name():
     print("Please enter your name:")
     user_name = input()
 
     print(f"Hello {user_name}!")
+
+
+def get_random_or_specific():
+    global goto_random_planet
 
     print("Would you like to go to a random planet? (Y/N)")
     goto_random_planet = input().strip().lower()
@@ -18,21 +27,28 @@ def main():
         print("Please enter either Y or N.")
         goto_random_planet = input().strip().lower()
 
+
+def print_planet_details():
     with open("planetarySystem.json", "r") as planets_json:
         planets_json = json.load(planets_json)
 
         if goto_random_planet == 'y':
             print("Choosing a random planet")
-            
-            print("Travelling to Earth")
-            print(planets_json["planets"][2]["description"])
         else:
             print("What planet would you like to go to?")
             goto_planet = input()
             # TODO Check for valid planet name
 
-            print("Travelling to Earth")
-            print(planets_json["planets"][2]["description"])
+        # TODO Randomly select a planet or use a given planet
+        print("Travelling to Earth")
+        print(planets_json["planets"][2]["description"])
+
+
+def main():
+    print_welcome()
+    get_name()
+    get_random_or_specific()
+    print_planet_details()
 
 
 main()
