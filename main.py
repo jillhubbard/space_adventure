fa8bd20de93ab837f580fb7defe335b302024467
Xander Bailey <xander.bailey@gmail.com>
2021-11-21 17:27:58 -0800

    Print planet details

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 A simple program to explore the solar system a bit.
 """
 
+import json
+
 print("Welcome to the Solar System!")
 
 print("Please enter your name:")
@@ -13,15 +15,19 @@ print("Would you like to go to a random planet? (Y/N)")
 goto_random_planet = input()
 # TODO Check for valid input
 
-if goto_random_planet:
-    print("Choosing a random planet")
-    
-    print("Travelling to ")
-    # TODO Print planet details
-else:
-    print("What planet would you like to go to?")
-    goto_planet = input()
-    # TODO Check for valid planet name
+with open("planetarySystem.json", "r") as planets_json:
+    planets_json = json.load(planets_json)
 
-    print("Travelling to ")
+    if goto_random_planet:
+        print("Choosing a random planet")
+        
+        print("Travelling to Earth")
+        print(planets_json["planets"][2]["description"])
+    else:
+        print("What planet would you like to go to?")
+        goto_planet = input()
+        # TODO Check for valid planet name
+
+        print("Travelling to Earth")
+        print(planets_json["planets"][2]["description"])
 
