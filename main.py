c57d0fc2ba80e696ee20702dcfcc2b860a8f20de
Xander Bailey <xander.bailey@gmail.com>
2021-11-21 17:14:56 -0800

    General outline

--- main.py
+++ main.py
@@ -2,6 +2,26 @@
 A simple program to explore the solar system a bit.
 """
 
-def main():
-    pass
+print("Welcome to the Solar System!")
+
+print("Please enter your name:")
+user_name = input()
+
+print(f"Hello {user_name}!")
+
+print("Would you like to go to a random planet? (Y/N)")
+goto_random_planet = input()
+# TODO Check for valid input
+
+if goto_random_planet:
+    print("Choosing a random planet")
+    
+    print("Travelling to ")
+    # TODO Print planet details
+else:
+    print("What planet would you like to go to?")
+    goto_planet = input()
+    # TODO Check for valid planet name
+
+    print("Travelling to ")
 
