1ee25551e8cdd83e1a7d95f7c4dc1e1d60efcb9d
Xander Bailey <xander.bailey@gmail.com>
2021-11-21 17:33:18 -0800

    Wrap in main funciton

--- main.py
+++ main.py
@@ -4,32 +4,35 @@ A simple program to explore the solar system a bit.
 
 import json
 
-print("Welcome to the Solar System!")
+def main():
+    print("Welcome to the Solar System!")
 
-print("Please enter your name:")
-user_name = input()
+    print("Please enter your name:")
+    user_name = input()
 
-print(f"Hello {user_name}!")
+    print(f"Hello {user_name}!")
 
-print("Would you like to go to a random planet? (Y/N)")
-goto_random_planet = input().strip().lower()
-while goto_random_planet not in 'yn':
-    print("Please enter either Y or N.")
+    print("Would you like to go to a random planet? (Y/N)")
     goto_random_planet = input().strip().lower()
+    while goto_random_planet not in 'yn':
+        print("Please enter either Y or N.")
+        goto_random_planet = input().strip().lower()
 
-with open("planetarySystem.json", "r") as planets_json:
-    planets_json = json.load(planets_json)
+    with open("planetarySystem.json", "r") as planets_json:
+        planets_json = json.load(planets_json)
 
-    if goto_random_planet == 'y':
-        print("Choosing a random planet")
-        
-        print("Travelling to Earth")
-        print(planets_json["planets"][2]["description"])
-    else:
-        print("What planet would you like to go to?")
-        goto_planet = input()
-        # TODO Check for valid planet name
+        if goto_random_planet == 'y':
+            print("Choosing a random planet")
+            
+            print("Travelling to Earth")
+            print(planets_json["planets"][2]["description"])
+        else:
+            print("What planet would you like to go to?")
+            goto_planet = input()
+            # TODO Check for valid planet name
 
-        print("Travelling to Earth")
-        print(planets_json["planets"][2]["description"])
+            print("Travelling to Earth")
+            print(planets_json["planets"][2]["description"])
 
+
+main()
